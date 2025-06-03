82ad67dc1967d14d67708055bcad45231346fabd
developedbybarley@gmail.com <71340344+DevelopedByBarley@users.noreply.github.com>
2025-06-03 10:28:05 +0200

    learn laravel 1

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,5 +1,12 @@
-@props(['active' => false])
+@props(['active' => false, 'type' => 'a'])
 
-<a class="{{ $active ? 'block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white' : 'block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white' }}"
-    aria-current="{{ $active ? 'page ' : 'false' }}" {{ $attributes }}> {{ $slot }}
-</a>
+
+@if ($type === 'a')
+    <a class="{{ $active ? 'block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white' : 'block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white' }}"
+        aria-current="{{ $active ? 'page ' : 'false' }}" {{ $attributes }}> {{ $slot }}
+        <a />
+    @else
+        <button class="{{ $active ? 'block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white' : 'block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white' }}"
+            aria-current="{{ $active ? 'page ' : 'false' }}" {{ $attributes }}> {{ $slot }}
+            <button />
+@endif
